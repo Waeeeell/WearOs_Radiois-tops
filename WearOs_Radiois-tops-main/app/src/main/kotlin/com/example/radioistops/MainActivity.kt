27282315cd56545877fed01e71bd5752af99e705
Waeeeell <waelelk053@gmail.com>
2026-04-01 02:48:36 +0200

    panatlla negra sol3

--- WearOs_Radiois-tops-main/app/src/main/kotlin/com/example/radioistops/MainActivity.kt
+++ WearOs_Radiois-tops-main/app/src/main/kotlin/com/example/radioistops/MainActivity.kt
@@ -9,13 +9,24 @@ import com.example.radioistops.presentation.navigation.WearNavGraph
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        android.util.Log.e("DEBUG_WEAR", "¡MainActivity HA ARRANCADO CORRETAMENTE!")
         
         setContent {
             MaterialTheme {
-                androidx.wear.compose.material.Scaffold(
-                    timeText = { androidx.wear.compose.material.TimeText() }
-                ) {
-                    WearNavGraph()
+                // REQUIRED ON WEAR OS: Si la app no tiene un componente deslizable nativo
+                // el sistema puede bloquear los frames visuales de Compose creyendo que no tiene root válido.
+                val state = androidx.wear.compose.material.rememberSwipeToDismissBoxState()
+                androidx.wear.compose.material.SwipeToDismissBox(
+                    state = state,
+                    onDismissed = { finish() }
+                ) { isBackground ->
+                    if (!isBackground) {
+                        androidx.wear.compose.material.Scaffold(
+                            timeText = { androidx.wear.compose.material.TimeText() }
+                        ) {
+                            WearNavGraph()
+                        }
+                    }
                 }
             }
         }
